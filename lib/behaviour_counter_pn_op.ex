bbab90f82f778f90f7b934c7bb43822de185561a
ubuntu <ubuntu@ubuntu.fritz.box>
2025-07-30 15:23:35 +0200

    Fix: Removed duplicate @doc attributes from behaviour_counter_pn_op.ex and behaviour_set_aw_op.ex

--- lib/behaviour_counter_pn_op.ex
+++ lib/behaviour_counter_pn_op.ex
@@ -27,46 +27,28 @@ defmodule PositiveNegativeCounter do
     positive_sum - negative_sum
   end
 
-  @doc """
-  Generates an increment effect with the given value.
-  """
   def generate_effect({:increment, value}, _state) do
     {:ok, {:inc_effect, value}}
   end
 
-  @doc """
-  Generates a decrement effect with the given value.
-  """
   def generate_effect({:decrement, value}, _state) do
     {:ok, {:dec_effect, value}}
   end
 
-  @doc """
-  Applies an increment effect to the PN-Counter by incrementing the positive vector clock.
-  """
   def apply_effect({{:inc_effect, value}, node_id}, {positive_vc, negative_vc}) do
     updated_positive_vc = Enum.reduce(1..value, positive_vc, fn _, acc -> Vector_Clock.increment(acc, node_id) end)
     {:ok, {updated_positive_vc, negative_vc}}
   end
 
-  @doc """
-  Applies a decrement effect to the PN-Counter by incrementing the negative vector clock.
-  """
   def apply_effect({{:dec_effect, value}, node_id}, {positive_vc, negative_vc}) do
     updated_negative_vc = Enum.reduce(1..value, negative_vc, fn _, acc -> Vector_Clock.increment(acc, node_id) end)
     {:ok, {positive_vc, updated_negative_vc}}
   end
 
-  @doc """
-  Indicates that this CRDT does not require state for effect generation.
-  """
   def requires_state_for_effect(_operation_payload) do
     false
   end
 
-  @doc """
-  Checks if two PN-Counter states are equal.
-  """
   def are_equal(state_a, state_b) do
     state_a == state_b
   end
--- lib/behaviour_set_aw_op.ex
+++ lib/behaviour_set_aw_op.ex
@@ -24,17 +24,10 @@ defmodule AddWinsSet do
     Map.keys(current_state) |> MapSet.new()
   end
 
-  @doc """
-  Generates an add effect for the given item.
-  """
   def generate_effect({:add_element, item_to_add}, _current_state) do
     {:ok, {:add_effect, item_to_add}}
   end
 
-  @doc """
-  Generates a remove effect for the given item.
-  Returns an error if the item is not found in the current state.
-  """
   def generate_effect({:remove_element, item_to_remove}, current_state) do
     if Map.has_key?(current_state, item_to_remove) do
       {:ok, {:remove_effect, {item_to_remove, Map.get(current_state, item_to_remove)}}}
@@ -44,19 +37,11 @@ defmodule AddWinsSet do
   end
 
   # Handle updates with explicit node information
-  @doc """
-  Applies an add effect to the Add-Wins Set.
-  Increments the version clock for the item in the current state.
-  """
   def apply_effect({{:add_effect, item}, node_id}, current_state) do
     version_clock = Map.get(current_state, item, Vector_Clock.new())
     {:ok, Map.put(current_state, item, Vector_Clock.increment(version_clock, node_id))}
   end
 
-  @doc """
-  Applies a remove effect to the Add-Wins Set.
-  Removes the item from the current state only if the removal's version clock dominates the item's version clock.
-  """
   def apply_effect({{:remove_effect, {item, removal_vc}}, _node_id}, current_state) do
     case Map.get(current_state, item) do
       nil ->
@@ -74,21 +59,13 @@ defmodule AddWinsSet do
     end
   end
 
-  @doc """
-  Applies an add effect to the Add-Wins Set (without explicit node ID).
-  Increments the version clock for the item in the current state using the current node's ID.
-  """
-  def apply_effect({:add_effect, item}, current_state) do
+  def apply_effect({:add_element, item}, current_state) do
     current_node = node()
     version_clock = Map.get(current_state, item, Vector_Clock.new())
     {:ok, Map.put(current_state, item, Vector_Clock.increment(version_clock, current_node))}
   end
 
-  @doc """
-  Applies a remove effect to the Add-Wins Set (without explicit node ID).
-  Removes the item from the current state only if the removal's version clock dominates the item's version clock.
-  """
-  def apply_effect({:remove_effect, {item, removal_vc}}, current_state) do
+  def apply_effect({:remove_element, {item, removal_vc}}, current_state) do
     case Map.get(current_state, item) do
       nil ->
         # Item not present, nothing to remove
@@ -103,23 +80,14 @@ defmodule AddWinsSet do
     end
   end
 
-  @doc """
-  Indicates that an add element operation does not require state for effect generation.
-  """
   def requires_state_for_effect({:add_element, _}) do
     false
   end
 
-  @doc """
-  Indicates that a remove element operation requires state for effect generation.
-  """
   def requires_state_for_effect({:remove_element, _}) do
     true
   end
 
-  @doc """
-  Checks if two Add-Wins Set states are equal.
-  """
   def are_equal(state_one, state_two) do
     state_one == state_two
   end
